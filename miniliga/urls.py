da3b524a7a24ee697975078432e41156462385f8
DropD <r.haeuselmann@gmx.ch>
2021-12-04 18:35:18 +0100

    add pre-commit checks and tools setup file

--- miniliga/urls.py
+++ miniliga/urls.py
@@ -1,4 +1,6 @@
-"""miniliga URL Configuration
+# -*- coding: utf-8 -*-
+"""
+miniliga URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -17,5 +19,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
