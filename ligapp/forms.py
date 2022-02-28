8e91950ad8fce767b21bfb3018ef90542a8e14f4
DropD <r.haeuselmann@gmx.ch>
2022-02-28 22:16:09 +0100

    [wip] switch new-match-form to crispy forms

--- ligapp/forms.py
+++ ligapp/forms.py
@@ -1,4 +1,7 @@
 """Forms for the ligapp."""
+from crispy_bootstrap5.bootstrap5 import FloatingField
+from crispy_forms import layout  # noqa: I900 # comes from django-crispy-forms
+from crispy_forms.helper import FormHelper  # noqa: I900
 from django import forms
 from django.core import validators
 from django.core.exceptions import ValidationError
@@ -30,16 +33,24 @@ class NewMatchForm(forms.Form):
     season = forms.ModelChoiceField(
         queryset=Season.objects, disabled=True, widget=forms.HiddenInput()
     )
-    first_player = forms.ModelChoiceField(queryset=Player.objects)
-    second_player = forms.ModelChoiceField(queryset=Player.objects)
+    first_player = forms.ModelChoiceField(
+        queryset=Player.objects, empty_label="Select a Player"
+    )
+    second_player = forms.ModelChoiceField(
+        queryset=Player.objects, empty_label="Select a Player"
+    )
     match_type = forms.ChoiceField(choices=MatchType.choices, initial=MatchType.SETS)
-    date_played = forms.DateTimeField()
-    first_score_1 = ScoreField()
-    second_score_1 = ScoreField()
-    first_score_2 = ScoreField(required=False)
-    second_score_2 = ScoreField(required=False)
-    first_score_3 = ScoreField(required=False)
-    second_score_3 = ScoreField(required=False)
+    date_played = forms.DateField()
+    first_score_1 = ScoreField(label="Score", localize=True)
+    second_score_1 = ScoreField(label="Score", localize=True)
+    first_score_2 = ScoreField(label="Score", required=False, localize=True)
+    second_score_2 = ScoreField(label="Score", required=False, localize=True)
+    first_score_3 = ScoreField(label="Score", required=False, localize=True)
+    second_score_3 = ScoreField(label="Score", required=False, localize=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = self.get_helper()
 
     def clean(self):
         cleaned_data = super().clean()
@@ -72,3 +83,97 @@ class NewMatchForm(forms.Form):
                         _("Is not a participant in this season."), code="invalid"
                     ),
                 )
+
+    def get_helper(self):
+        helper = FormHelper()
+        helper.add_input(layout.Submit("submit", "Save", css_class="btn btn-success"))
+        helper.add_input(layout.Button("cancel", "Cancel", css_class="btn btn-danger"))
+        helper.layout = layout.Layout(
+            "Info",
+            layout.Fieldset(
+                "",
+                FloatingField("first_player", css_class="match-input"),
+                layout.HTML("<div class='match-input player-vs'>vs.</div>"),
+                FloatingField("second_player", css_class="match-input"),
+            ),
+            layout.Fieldset(
+                "",
+                FloatingField("match_type", css_class="match-input"),
+                FloatingField(
+                    "date_played", css_class="match-input", id="dateplayedpicker"
+                ),
+                layout.HTML(
+                    "<script type='text/javascript'>"
+                    "const datepicker = new tempusDominus.TempusDominus("
+                    "document.getElementById('div_id_date_played'), "
+                    "{localization: {locale: 'de' }, "
+                    "display: {components: {date: true, decades: true, month: true, year: true, hours: false, minutes: false, seconds: false}}}"
+                    ");"
+                    "</script>"
+                ),
+            ),
+            layout.Fieldset(
+                "",
+                layout.Div(
+                    FloatingField(
+                        "first_score_1",
+                        label="Score",
+                        css_class="match-input score-input",
+                    ),
+                    css_class="col",
+                ),
+                layout.HTML("<div class='col score-vs'>:</div>"),
+                layout.Div(
+                    FloatingField(
+                        "second_score_1",
+                        label="Score",
+                        css_class="match-input score-input",
+                    ),
+                    css_class="col",
+                ),
+                css_class="row g-0 match-input",
+            ),
+            layout.Fieldset(
+                "",
+                layout.Div(
+                    FloatingField(
+                        "first_score_2",
+                        label="Score",
+                        css_class="match-input score-input",
+                    ),
+                    css_class="col",
+                ),
+                layout.HTML("<div class='col score-vs'>:</div>"),
+                layout.Div(
+                    FloatingField(
+                        "second_score_2",
+                        label="Score",
+                        css_class="match-input score-input",
+                    ),
+                    css_class="col",
+                ),
+                css_class="row g-0 match-input",
+            ),
+            layout.Fieldset(
+                "",
+                layout.Div(
+                    FloatingField(
+                        "first_score_3",
+                        label="Score",
+                        css_class="match-input score-input",
+                    ),
+                    css_class="col",
+                ),
+                layout.HTML("<div class='col score-vs'>:</div>"),
+                layout.Div(
+                    FloatingField(
+                        "second_score_3",
+                        label="Score",
+                        css_class="match-input score-input",
+                    ),
+                    css_class="col",
+                ),
+                css_class="row g-0 match-input",
+            ),
+        )
+        return helper
--- miniliga/settings.py
+++ miniliga/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "fontawesomefree",
 ]
 
 MIDDLEWARE = [
@@ -125,5 +128,9 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 django_heroku.settings(locals())
