154554c49d1b02bfbb3381c9732c8b5538877368
DropD <r.haeuselmann@gmx.ch>
2022-03-01 23:07:21 +0100

    localize datetime input format also in validation

--- ligapp/forms.py
+++ ligapp/forms.py
@@ -1,4 +1,6 @@
 """Forms for the ligapp."""
+from datetime import datetime
+
 from crispy_bootstrap5.bootstrap5 import FloatingField
 from crispy_forms import layout  # noqa: I900 # comes from django-crispy-forms
 from crispy_forms.helper import FormHelper  # noqa: I900
@@ -6,6 +8,7 @@ from django import forms
 from django.core import validators
 from django.core.exceptions import ValidationError
 from django.db.models import TextChoices
+from django.utils.formats import get_format
 from django.utils.translation import gettext as _
 
 from .models import Player, Season
@@ -27,6 +30,24 @@ class ScoreField(forms.IntegerField):
     ]
 
 
+class DatePickerField(forms.DateField):
+    """Field for use with TempusDominus date picker."""
+
+    def __init__(self, *args, lang, **kwargs):
+        """Add the lang keyword for date localization."""
+        super().__init__(*args, **kwargs)
+        self.lang = lang
+
+    def to_python(self, value):
+        for format in get_format("DATE_INPUT_FORMATS", self.lang):
+            try:
+                print(f"trying {format}")
+                return datetime.strptime(value, format).date()
+            except ValueError:
+                continue
+        raise ValidationError(_("Invalid date."), code="invalid")
+
+
 class NewMatchForm(forms.Form):
     """Form for recording a match."""
 
@@ -40,7 +61,7 @@ class NewMatchForm(forms.Form):
         queryset=Player.objects, empty_label="Select a Player"
     )
     match_type = forms.ChoiceField(choices=MatchType.choices, initial=MatchType.SETS)
-    date_played = forms.DateField()
+    date_played = DatePickerField(lang="de-ch")
     first_score_1 = ScoreField(label="Score", localize=True)
     second_score_1 = ScoreField(label="Score", localize=True)
     first_score_2 = ScoreField(label="Score", required=False, localize=True)
@@ -49,6 +70,7 @@ class NewMatchForm(forms.Form):
     second_score_3 = ScoreField(label="Score", required=False, localize=True)
 
     def __init__(self, *args, **kwargs):
+        """Add the helper instance attr."""
         super().__init__(*args, **kwargs)
         self.helper = self.get_helper()
 
@@ -88,6 +110,7 @@ class NewMatchForm(forms.Form):
         helper = FormHelper()
         helper.add_input(layout.Submit("submit", "Save", css_class="btn btn-success"))
         helper.add_input(layout.Button("cancel", "Cancel", css_class="btn btn-danger"))
+        date_lang = self.fields["date_played"].lang
         helper.layout = layout.Layout(
             "Info",
             layout.Fieldset(
@@ -106,8 +129,9 @@ class NewMatchForm(forms.Form):
                     "<script type='text/javascript'>"
                     "const datepicker = new tempusDominus.TempusDominus("
                     "document.getElementById('div_id_date_played'), "
-                    "{localization: {locale: 'de' }, "
-                    "display: {components: {date: true, decades: true, month: true, year: true, hours: false, minutes: false, seconds: false}}}"
+                    f"{{localization: {{locale: '{date_lang}' }}, "
+                    "display: {components: {date: true, decades: true, month: true, "
+                    "year: true, hours: false, minutes: false, seconds: false}}}"
                     ");"
                     "</script>"
                 ),
