e138b92637b3f2c5479a397f46eaa7616a7ecf99
DropD <r.haeuselmann@gmx.ch>
2022-09-06 12:13:12 +0200

    fix more browsertests, migrate

--- tests/plan_match_workflow.feature
+++ tests/plan_match_workflow.feature
@@ -1,23 +1,23 @@
 Feature: Plan and record Matches
 
-  Scenario: Plan matches
+  Scenario Outline: Plan matches
 
     Given I am logged in on the planning feature season page
     When I plan a match between <player_1> and <player_2>
     Then I should see the match between <player_1> and <player_2> in the planned matches list
 
     Examples:
-      | player_1 | player_2 |
-      | Mark     | Lee      |
-      | Steve    | Dan      |
+    | player_1 | player_2 |
+    | Mark     | Lee      |
+    | Steve    | Dan      |
 
-  Scenario: Record results
+  Scenario Outline: Record results
 
     Given There is a planned match between <player_1> and <player_2>
     When I record the result for <player_1> and <player_2> as <p11> : <p21>, <p12> : <p22>
     Then I should see the match between <player_1> and <player_2> with <p11> : <p21>, <p12> : <p22> in the played matches list
 
     Examples:
-      | player_1 | p11 | p12 | player_2 | p21 | p22 |
-      | Mark     | 21  | 21  | Lee      | 14  | 17  |
-      | Steve    | 13  | 11  | Dan      | 21  | 21  |
+    | player_1 | p11 | p12 | player_2 | p21 | p22 |
+    | Mark     | 21  | 21  | Lee      | 14  | 17  |
+    | Steve    | 13  | 11  | Dan      | 21  | 21  |
--- tests/test_new_match.py
+++ tests/test_new_match.py
@@ -1,6 +1,8 @@
 """Test steps for adding a new match."""
-from pytest_bdd import scenario, then, when  # noqa: I900  # dev requirements
-from selenium.webdriver.common.keys import Keys  # noqa: I900  # dev requirements
+from pytest_bdd import scenario, then, when
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import select, wait
 
 
 @scenario("new_match.feature", "Add a new match to an existing season")
@@ -38,89 +40,119 @@ def browse_to_seasons_list(
     authbrowser,
 ):
     """Click on the season link and ensure we got to the details page."""
-    authbrowser.links.find_by_text(season_name).click()
-    assert authbrowser.find_by_tag("h1").first.text == season_name
+    seasons = authbrowser.find_elements(By.TAG_NAME, "a")
+    [s for s in seasons if s.text == season_name][0].click()
+    wait.WebDriverWait(authbrowser, 3).until(
+        lambda b: b.find_element(By.TAG_NAME, "h1").text == season_name
+    )
+    assert authbrowser.find_element(By.TAG_NAME, "h1").text == season_name
 
 
 @when("I click to add a match")
 def click_add_match(authbrowser):
-    assert not authbrowser.find_by_text("Kento vs. Victor")
-    authbrowser.find_by_id("button-new-match").click()
+    matches = authbrowser.find_elements(By.CSS_SELECTOR, ".match.row")
+    assert ("Kento", "Victor") not in [
+        (p.text for p in m.find_elements(By.CSS_SELECTOR, ".match-player"))
+        for m in matches
+    ]
+    authbrowser.find_element(By.ID, "button-new-match").click()
 
 
 @when("I enter valid data and submit")
 def enter_valid_match_data(authbrowser):
-    authbrowser.find_by_id("select2-id_first_player-container").first.click()
-    authbrowser.find_by_css(".select2-search__field").type("Ken" + Keys.RETURN)
-    authbrowser.find_by_id("select2-id_second_player-container").first.click()
-    authbrowser.find_by_css(".select2-search__field").type("Vic" + Keys.RETURN)
-    authbrowser.select("match_type", "Points")
-    authbrowser.fill("date_played", "1.3.2020")
-    authbrowser.fill("first_score_1", 21)
-    authbrowser.fill("second_score_1", 19)
-    authbrowser.fill("first_score_2", 21)
-    authbrowser.fill("second_score_2", 11)
-    authbrowser.find_by_name("submit").first.click()
+    authbrowser.find_element(By.ID, "select2-id_first_player-container").click()
+    authbrowser.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(
+        "Ken" + Keys.RETURN
+    )
+    authbrowser.find_element(By.ID, "select2-id_second_player-container").click()
+    authbrowser.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(
+        "Vic" + Keys.RETURN
+    )
+    select.Select(
+        authbrowser.find_element(By.NAME, "match_type")
+    ).select_by_visible_text("Sets")
+    date_input = authbrowser.find_element(By.NAME, "date_played")
+    date_input.clear()
+    date_input.send_keys("1.3.2020")
+    authbrowser.find_element(By.NAME, "first_score_1").send_keys(21)
+    authbrowser.find_element(By.NAME, "second_score_1").send_keys(19)
+    authbrowser.find_element(By.NAME, "first_score_2").send_keys(21)
+    authbrowser.find_element(By.NAME, "second_score_2").send_keys(11)
+    authbrowser.find_element(By.NAME, "submit").click()
 
 
 @when("I enter valid data for a fixed duration match and submit")
 def enter_valid_timed_match_data(authbrowser):
-    authbrowser.find_by_id("select2-id_first_player-container").first.click()
-    authbrowser.find_by_css(".select2-search__field").type("Vic" + Keys.RETURN)
-    authbrowser.find_by_id("select2-id_second_player-container").first.click()
-    authbrowser.find_by_css(".select2-search__field").type("Ken" + Keys.RETURN)
-    authbrowser.select("match_type", "Time")
-    authbrowser.fill("minutes_played", "10")
-    authbrowser.fill("date_played", "1.4.2020")
-    authbrowser.fill("first_score_1", 54)
-    authbrowser.fill("second_score_1", 32)
-    authbrowser.find_by_name("submit").first.click()
+    authbrowser.find_element(By.ID, "select2-id_first_player-container").click()
+    authbrowser.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(
+        "Vic" + Keys.RETURN
+    )
+    authbrowser.find_element(By.ID, "select2-id_second_player-container").click()
+    authbrowser.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(
+        "Ken" + Keys.RETURN
+    )
+    select.Select(
+        authbrowser.find_element(By.NAME, "match_type")
+    ).select_by_visible_text("Time")
+    authbrowser.find_element(By.NAME, "minutes_played").send_keys("10")
+    date_input = authbrowser.find_element(By.NAME, "date_played")
+    date_input.clear()
+    date_input.send_keys("1.4.2020")
+    authbrowser.find_element(By.NAME, "first_score_1").send_keys(54)
+    authbrowser.find_element(By.NAME, "second_score_1").send_keys(32)
+    authbrowser.find_element(By.NAME, "submit").click()
 
 
 @when("I click cancel")
 def cancel_new_match(authbrowser):
-    authbrowser.find_link_by_text("Cancel").click()
+    [e for e in authbrowser.find_elements(By.CLASS_NAME, "btn") if e.text == "Cancel"][
+        0
+    ].click()
 
 
 @then("The new match should be in the list")
-def new_match_in_list(
-    authbrowser,
-):
-    matches = authbrowser.find_by_css(".match")
+def new_match_in_list(authbrowser):
+    matches = authbrowser.find_elements(By.CSS_SELECTOR, ".match")
     new_match = None
     for match in matches:
-        players = match.find_by_css(".match-players")
+        players = match.find_element(By.CSS_SELECTOR, ".match-players")
         if players.text == "Kento\nVictor":
             new_match = match
             break
     assert new_match
-    scores = [i.text for i in new_match.find_by_css(".match-score")]
+    scores = [i.text for i in new_match.find_elements(By.CSS_SELECTOR, ".match-score")]
     assert scores == ["21\n19", "21\n11"]
 
 
 @then("The new fixed duration match should be in the list")
-def new_timed_match_in_list(
-    authbrowser,
-):
-    matches = authbrowser.find_by_css(".match")
+def new_timed_match_in_list(authbrowser):
+    matches = authbrowser.find_elements(By.CSS_SELECTOR, ".match")
     new_match = None
     for match in matches:
-        players = match.find_by_css(".match-players")
+        players = match.find_element(By.CSS_SELECTOR, ".match-players")
         if players.text == "Victor\nKento":
             new_match = match
             break
     assert new_match
-    assert new_match.find_by_css(".match-duration").first.text == "10'"
-    scores = [i.text for i in new_match.find_by_css(".match-score")]
+    assert new_match.find_element(By.CSS_SELECTOR, ".match-duration").text == "10'"
+    scores = [i.text for i in new_match.find_elements(By.CSS_SELECTOR, ".match-score")]
     assert scores == ["54\n32"]
 
 
 @then("I should not see the add match button")
 def no_add_match(nonadmin_authbrowser):
-    assert not nonadmin_authbrowser.find_by_id("button-new-match")
-    assert not nonadmin_authbrowser.find_link_by_text("New Match")
+    assert not nonadmin_authbrowser.find_elements(By.ID, "button-new-match")
+    assert not [
+        link
+        for link in nonadmin_authbrowser.find_elements(By.TAG_NAME, "a")
+        if link.text == "New Match"
+    ]
 
 
 @then("I should not see any seasons")
 def season_not_in_list(nonadmin_authbrowser, season_name):
-    assert not nonadmin_authbrowser.links.find_by_text(season_name)
+    assert not [
+        link
+        for link in nonadmin_authbrowser.find_elements(By.TAG_NAME, "a")
+        if link.text == season_name
+    ]
