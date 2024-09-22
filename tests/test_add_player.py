b397cc37d7a34d950a59b89d0210ded09e56b629
DropD <rico.haeuselmann@gmail.com>
2024-09-22 22:39:05 +0200

    make player adding suggestions distinct.

--- tests/test_add_player.py
+++ tests/test_add_player.py
@@ -24,6 +24,11 @@ def test_no_crossadmin_suggestions(authbrowser, hidden_player_names):
     """Collect scenario steps."""
 
 
+@scenario("add_player.feature", "Add an existing player who is in multiple accessible seasons")
+def test_add_seasoned_player(authbrowser):
+    """Collect scenario steps."""
+
+
 @when("I click to add a player")
 def click_add_player(authbrowser):
     authbrowser.find_element(By.ID, "button-add-player").click()
@@ -48,6 +53,16 @@ def type_existing_name_submit(authbrowser):
     authbrowser.find_element(By.NAME, "submit").click()
 
 
+@when("I enter the name of the target player and submit")
+def type_target_name_submit(authbrowser):
+    authbrowser.find_element(By.ID, "select2-id_name-container").click()
+    search_field = authbrowser.find_element(By.CSS_SELECTOR, ".select2-search__field")
+    search_field.send_keys("Akane")
+    options = authbrowser.find_elements(By.CSS_SELECTOR, ".select2-results__option")
+    [o for o in options if o.text == "Akane"][0].click()
+    authbrowser.find_element(By.NAME, "submit").click()
+
+
 @when("I click the player name input")
 def click_player_name_input(authbrowser):
     authbrowser.find_element(By.ID, "select2-id_name-container").click()
@@ -61,6 +76,11 @@ def new_player_ranked(authbrowser):
 @then("the ranking should be updated with the added existing player")
 def added_player_ranked(authbrowser):
     assert authbrowser.find_elements(By.CSS_SELECTOR, "#season-ranking li")[3].text == "4. Christo"
+
+
+@then("the ranking should be updated with the added target player")
+def target_player_ranked(authbrowser):
+    assert authbrowser.find_elements(By.CSS_SELECTOR, "#season-ranking li")[4].text == "5. Akane"
 
 
 @then("No players from the other season should be suggested")
